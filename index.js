141d07786e8c6d7e45e040efb2c2a6c7a2bebe6b
Priya Mohan <priyamohan17@ufl.edu>
2023-02-05 19:43:15 -0500

    javascript

---
+++ index.js
@@ -0,0 +1,79 @@
+var words = [
+  'bananas',
+  'grapes',
+  'carousel',
+  'milkshake',
+  'javascript',
+  'limousine',
+  'chocolate',
+  'programming',
+  'meatloaf',
+  'ukulele',
+  'mango'
+]
+//get variables with IDs in HTML//
+var wordToGuess = document.getElementById("word-to-guess")
+var remainingGuesses = document.getElementById("remaining-guesses")
+var incorrectLettersShown = document.getElementById("incorrect-letters")
+var previousWord = document.getElementById("previous-word")
+var winsShown = document.getElementById("wins")
+var lossesShown = document.getElementById("losses")
+var correctLetters = []
+var incorrectLetters = []
+var letterGuessed = []
+var randomSelect
+var visibleWord
+var wins = 0
+var losses = 0 
+var guessesRemaining = 10
+
+//keyboard access//
+document.onkeyup = function(e) {
+  var key = e.key.toLowerCase()
+  if (randomSelect.includes(key) && correctLetters.indexOf(key) === -1) { 
+    correctLetters.push(key)
+  } else if (!randomSelect.includes(key) && incorrectLetters.indexOf(key) === -1) {
+    incorrectLetters.push(key)
+    guessesRemaining--
+  }
+ 
+  guesses()
+  if (visibleWord === randomSelect) {
+    wins++
+    winsShown.textContent = wins
+    previousWord.textContent = randomSelect
+
+    runGame()
+  }
+  if (guessesRemaining <= 0) { 
+    losses++
+    lossesShown.textContent = losses
+    previousWord.textContent = randomSelect
+   
+    runGame()
+    }}
+  function guesses() {
+      visibleWord = ''
+      for (let i = 0; i < randomSelect.length; i++) {
+        if (correctLetters.indexOf(randomSelect[i]) > -1) {
+          visibleWord += randomSelect[i]
+        } else {
+          visibleWord += '_'
+        }
+      }
+      remainingGuesses.textContent = guessesRemaining
+      incorrectLettersShown.textContent = incorrectLetters
+      wordToGuess.textContent = visibleWord
+      }   
+   //Need functio   
+  function runGame () {
+        randomSelect = words[Math.floor(Math.random()*words.length)]
+        correctLetters = randomSelect.split()
+        incorrectLetters = []
+        guessesRemaining = 10
+        
+        guesses()
+      }
+
+ 
+    runGame()
